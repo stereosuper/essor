045d1086c1bde4f9b3836a8c55909eb7b4d170fd
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-07-24 15:56:53 +0200

    :art: display footer infos

--- src/theme/footer.php
+++ src/theme/footer.php
@@ -1,6 +1,19 @@
         </main>
 
         <footer role='contentinfo'>
+            <?php the_field('offersTitle', 'options'); ?>
+            <?php the_field('offersSubtitle', 'options'); ?>
+            <a href='<?php the_field('offersPage', 'options'); ?>' class='btn'><?php the_field('offersBtn', 'options'); ?></a>
+
+            <?php the_field('newsTitle', 'options'); ?>
+            <?php the_field('newsSubtitle', 'options'); ?>
+
+            <?php the_field('officesTitle', 'options'); ?>
+            <?php the_field('officesSubtitle', 'options'); ?>
+            <a href='<?php the_field('officesPage', 'options'); ?>' class='btn'><?php the_field('officesBtn', 'options'); ?></a>
+
+            <span>&copy;<?php echo get_the_date('Y'); ?> - Groupe Essor</span>
+
             <?php wp_nav_menu( array( 'theme_location' => 'secondary', 'container' => false, 'menu_class' => 'menu-main' ) ); ?>
         </footer>
 
