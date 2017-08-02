96dac35bfcfb8305e75ca9052c3219f4f16ea171
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-08-02 18:00:37 +0200

    :art: references

---
+++ src/theme/references.php
@@ -0,0 +1,176 @@
+<?php 
+/*
+Template Name: Références
+*/
+
+get_header(); ?>
+
+    <?php if ( have_posts() ) : the_post(); ?>
+        <div class='container'>
+            <h1>Références</h1>
+            <p>Essor est riche d’une histoire : l’histoire de toutes les  filiales qui constituent aujourd’hui son identité. Elles se déploient depuis plus de 30 ans au contact <strong>d’une grande diversité de clients et de projets.</strong></p>
+            <div class='filters'>
+                <span class='title'>Filter par type :</span>
+                <ul>
+                    <li>
+                        <a href='#'>Bureaux</a>
+                    </li>
+                    <li>
+                        <a href='#'>Usines</a>
+                    </li>
+                    <li>
+                        <a href='#'>Locaux d'activités</a>
+                    </li>
+                    <li>
+                        <a href='#'>Entrepôts</a>
+                    </li>
+                    <li>
+                        <a href='#'>Hôtels</a>
+                    </li>
+                    <li>
+                        <a href='#'>Hypermarchés</a>
+                    </li>
+                    <li>
+                        <a href='#'>Centres commerciaux</a>
+                    </li>
+                    <li>
+                        <a href='#'>Zones d'activités</a>
+                    </li>
+                </ul>
+            </div>
+            <ul class='projects complete'>
+                <li>
+                    <a href='#'>
+                        <span class='wrapper-img'>
+                            <span class='img' style='background-image: url("<?php echo get_stylesheet_directory_uri();?>/img/visuel-ref.jpg")'></span>
+                        </span>
+                        <h3>Parc de stationnement pour le groupe Total<span>Pau (64)</span></h3>
+                    </a>
+                    <ul>
+                        <li><a href='#'>Essor immobilière</a></li>
+                        <li><a href='#'>Bureaux</a></li>
+                        <li><a href='#'>2016</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href='#'>
+                        <span class='wrapper-img'>
+                            <span class='img' style='background-image: url("<?php echo get_stylesheet_directory_uri();?>/img/visuel-ref.jpg")'></span>
+                        </span>
+                        <h3>Oceanet<span>Saint-Herblain (44)</span></h3>
+                    </a>
+                    <ul>
+                        <li><a href='#'>Essor immobilière</a></li>
+                        <li><a href='#'>Bureaux</a></li>
+                        <li><a href='#'>2014</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href='#'>
+                        <span class='wrapper-img'>
+                            <span class='img' style='background-image: url("<?php echo get_stylesheet_directory_uri();?>/img/visuel-ref.jpg")'></span>
+                        </span>
+                        <h3>Agence Pôle Emploi à Pau Blum<span>Pau Blum (64)</span></h3>
+                    </a>
+                    <ul>
+                        <li><a href='#'>Essor immobilière</a></li>
+                        <li><a href='#'>Bureaux</a></li>
+                        <li><a href='#'>2015</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href='#'>
+                        <span class='wrapper-img'>
+                            <span class='img' style='background-image: url("<?php echo get_stylesheet_directory_uri();?>/img/visuel-ref.jpg")'></span>
+                        </span>
+                        <h3>Siège de Varel Europe à Pau<span>Pau (64)</span></h3>
+                    </a>
+                    <ul>
+                        <li><a href='#'>Essor immobilière</a></li>
+                        <li><a href='#'>Bureaux</a></li>
+                        <li><a href='#'>2011</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href='#'>
+                        <span class='wrapper-img'>
+                            <span class='img' style='background-image: url("<?php echo get_stylesheet_directory_uri();?>/img/visuel-ref.jpg")'></span>
+                        </span>
+                        <h3>Parc de stationnement pour le groupe Total<span>Pau (64)</span></h3>
+                    </a>
+                    <ul>
+                        <li><a href='#'>Essor immobilière</a></li>
+                        <li><a href='#'>Bureaux</a></li>
+                        <li><a href='#'>2016</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href='#'>
+                        <span class='wrapper-img'>
+                            <span class='img' style='background-image: url("<?php echo get_stylesheet_directory_uri();?>/img/visuel-ref.jpg")'></span>
+                        </span>
+                        <h3>Oceanet<span>Saint-Herblain (44)</span></h3>
+                    </a>
+                    <ul>
+                        <li><a href='#'>Essor immobilière</a></li>
+                        <li><a href='#'>Bureaux</a></li>
+                        <li><a href='#'>2014</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href='#'>
+                        <span class='wrapper-img'>
+                            <span class='img' style='background-image: url("<?php echo get_stylesheet_directory_uri();?>/img/visuel-ref.jpg")'></span>
+                        </span>
+                        <h3>Agence Pôle Emploi à Pau Blum<span>Pau Blum (64)</span></h3>
+                    </a>
+                    <ul>
+                        <li><a href='#'>Essor immobilière</a></li>
+                        <li><a href='#'>Bureaux</a></li>
+                        <li><a href='#'>2015</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href='#'>
+                        <span class='wrapper-img'>
+                            <span class='img' style='background-image: url("<?php echo get_stylesheet_directory_uri();?>/img/visuel-ref.jpg")'></span>
+                        </span>
+                        <h3>Siège de Varel Europe à Pau<span>Pau (64)</span></h3>
+                    </a>
+                    <ul>
+                        <li><a href='#'>Essor immobilière</a></li>
+                        <li><a href='#'>Bureaux</a></li>
+                        <li><a href='#'>2011</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href='#'>
+                        <span class='wrapper-img'>
+                            <span class='img' style='background-image: url("<?php echo get_stylesheet_directory_uri();?>/img/visuel-ref.jpg")'></span>
+                        </span>
+                        <h3>Parc de stationnement pour le groupe Total<span>Pau (64)</span></h3>
+                    </a>
+                    <ul>
+                        <li><a href='#'>Essor immobilière</a></li>
+                        <li><a href='#'>Bureaux</a></li>
+                        <li><a href='#'>2016</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href='#'>
+                        <span class='wrapper-img'>
+                            <span class='img' style='background-image: url("<?php echo get_stylesheet_directory_uri();?>/img/visuel-ref.jpg")'></span>
+                        </span>
+                        <h3>Oceanet<span>Saint-Herblain (44)</span></h3>
+                    </a>
+                    <ul>
+                        <li><a href='#'>Essor immobilière</a></li>
+                        <li><a href='#'>Bureaux</a></li>
+                        <li><a href='#'>2014</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    <?php endif; ?>
+
+<?php get_footer(); ?>
