f1b56ecb09262030d3213efae06f70c1a25c8b97
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-08-03 12:30:33 +0200

    :art: refs

--- dest/wp-content/themes/essor/references.php
+++ dest/wp-content/themes/essor/references.php
@@ -6,37 +6,44 @@ Template Name: Références
 get_header(); ?>
 
     <?php if ( have_posts() ) : the_post(); ?>
-        <div class='container'>
-            <h1>Références</h1>
-            <p>Essor est riche d’une histoire : l’histoire de toutes les  filiales qui constituent aujourd’hui son identité. Elles se déploient depuis plus de 30 ans au contact <strong>d’une grande diversité de clients et de projets.</strong></p>
-            <div class='filters'>
-                <span class='title'>Filter par type :</span>
-                <ul>
-                    <li>
-                        <a href='#'>Bureaux</a>
-                    </li>
-                    <li>
-                        <a href='#'>Usines</a>
-                    </li>
-                    <li>
-                        <a href='#'>Locaux d'activités</a>
-                    </li>
-                    <li>
-                        <a href='#'>Entrepôts</a>
-                    </li>
-                    <li>
-                        <a href='#'>Hôtels</a>
-                    </li>
-                    <li>
-                        <a href='#'>Hypermarchés</a>
-                    </li>
-                    <li>
-                        <a href='#'>Centres commerciaux</a>
-                    </li>
-                    <li>
-                        <a href='#'>Zones d'activités</a>
-                    </li>
-                </ul>
+        <li class='container'>
+            <div class='wrapper-title'>
+                <div class='title'>
+                    <h1>Références</h1>
+                    <p>Essor est riche d’une histoire : l’histoire de toutes les  filiales qui constituent aujourd’hui son identité. Elles se déploient depuis plus de 30 ans au contact <strong>d’une grande diversité de clients et de projets.</strong></p>
+                </div>
+                <div class='filters'>
+                    <span class='title'>Filter</span>
+                    <form>
+                        <div class='select'>
+                            <select>
+                                <option value='' selected>Tout type de bâtiment</option>
+                                <option value=''>Bureaux</option>
+                                <option value=''>Usines</option>
+                                <option value=''>Locaux d'activités</option>
+                                <option value=''>Entrepôts</option>
+                                <option value=''>Hôtels</option>
+                                <option value=''>Hypermarchés</option>
+                                <option value=''>Centres commerciaux</option>
+                                <option value=''>Zones d'activités</option>
+                            </select>
+                            <svg class='icon icon-down'><use xlink:href='#icon-down'></use></svg>
+                        </div>
+                        <div class='select'>
+                            <select>
+                                <option value='' selected>Tous les métiers</option>
+                                <option value=''>Usines</option>
+                                <option value=''>Locaux d'activités</option>
+                                <option value=''>Entrepôts</option>
+                                <option value=''>Hôtels</option>
+                                <option value=''>Hypermarchés</option>
+                                <option value=''>Centres commerciaux</option>
+                                <option value=''>Zones d'activités</option>
+                            </select>
+                            <svg class='icon icon-down'><use xlink:href='#icon-down'></use></svg>
+                        </div>
+                    </form>
+                </div>
             </div>
             <ul class='projects complete'>
                 <li>
@@ -168,6 +175,9 @@ get_header(); ?>
                         <li><a href='#'>Bureaux</a></li>
                         <li><a href='#'>2014</a></li>
                     </ul>
+                </li>
+                <li class='load-more'>
+                    <a href='#'><span class='txt-more'>Charger la suite<svg class='icon icon-arrow-bottom'><use xlink:href='#icon-arrow-bottom'></use></svg></span></a>
                 </li>
             </ul>
         </div>
