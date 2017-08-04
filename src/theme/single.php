37c33549b2de9f4d10a56f9b00dbda8a6ffc5abd
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-08-04 17:30:56 +0200

    :art: ref detail

--- src/theme/single.php
+++ src/theme/single.php
@@ -3,18 +3,19 @@
 	<?php if ( have_posts() ) : ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
-
-			<article>
-					
-				<h1><?php the_title(); ?></h1>
-				<div class="postMeta">
-					<?php echo get_the_date(); ?>
-				</div>
-						
-				<?php the_content(); ?>
-						
-			</article>
-
+			<div class='wrapper-top-img small'>
+				<div class='top-img' style='background-image: url("<?php echo get_stylesheet_directory_uri();?>/img/visuel-ref.jpg")'></div>
+			</div>
+			<div class='container'>
+				<article>
+					<h1><?php the_title(); ?></h1>
+					<div class="postMeta">
+						<?php echo get_the_date(); ?>
+					</div>
+							
+					<?php the_content(); ?>	
+				</article>
+			</div>
 		<?php endwhile; ?>
 
 
