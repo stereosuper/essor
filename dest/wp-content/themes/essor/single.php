68225f6a7b73cb07756779dc0b94b49a319a7c92
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-08-07 17:02:47 +0200

    :art: single.php

--- dest/wp-content/themes/essor/single.php
+++ dest/wp-content/themes/essor/single.php
@@ -4,22 +4,19 @@
 
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class='wrapper-top-img small'>
-				<div class='top-img' style='background-image: url("<?php echo get_stylesheet_directory_uri();?>/img/visuel-ref.jpg")'></div>
+				<div class='top-img' style='background-image:url("<?php echo get_the_post_thumbnail_url();?>")'></div>
 			</div>
-			<div class='container-small'>
-				<article>
-					<div class='bloc-title-blog'>
-						<div class='postMeta'>En Mars 2017 dans <a href='#'>Chantier</a>, <a href='#'>Ingénierie</a></div>
-						<h1><?php the_title(); ?></h1>
-					</div>
-					<!-- <div class="postMeta">
-						<?php echo get_the_date(); ?>
-					</div> -->
-					<div class='blog-content'>
-						<?php the_content(); ?>
-					</div>
-				</article>
-			</div>
+			
+			<article class='container-small'>
+				<div class='bloc-title-blog'>
+					<div class='postMeta'>Le <?php echo get_the_date(); ?> dans <?php echo get_the_category_list(', '); ?></div>
+					<h1><?php the_title(); ?></h1>
+				</div>
+
+				<div class='blog-content'>
+					<?php the_content(); ?>
+				</div>
+			</article>
 		<?php endwhile; ?>
 
 
