fc8bf5c3000af81c13b3d2d3a5bcd2d2e45f90ff
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-08-08 15:03:36 +0200

    :art: search template

--- src/theme/search.php
+++ src/theme/search.php
@@ -1,25 +1,33 @@
 <?php get_header(); ?>
 
-	<?php if ( have_posts() ) : 
-	global $wp_query;
-	$results = $wp_query->found_posts; ?>
+	<div class='container'>
 
-		<h1>La recherche "<?php the_search_query(); ?>" a retourné <?php if($results > 1){ echo $results . ' résultats'; }else{ echo '1 résultat'; } ?> </h1>
+		<?php if ( have_posts() ) : 
+		global $wp_query;
+		$results = $wp_query->found_posts; ?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
+			<h1>La recherche "<?php the_search_query(); ?>" a retourné <?php if($results > 1){ echo $results . ' résultats'; }else{ echo '1 résultat'; } ?> </h1>
 
-			<h2><?php the_title(); ?></h2>
-			<?php the_excerpt(); ?>
-		
-		<?php endwhile; ?>
+			<ul>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<li>
+						<a href='<?php the_permalink(); ?>'>
+							<h2><?php the_title(); ?></h2>
+							<?php the_excerpt(); ?>
+						</a>
+					</li>
+				<?php endwhile; ?>
+			</ul>
 
 			<?php previous_posts_link('Résultats plus récents'); ?>
 			<?php next_posts_link('Résultats plus anciens'); ?>
-	
-	<?php else : ?>
-				
-		<h1>La recherche "<?php the_search_query(); ?>" n'a retourné aucun résultat</h1>
+		
+		<?php else : ?>
+					
+			<h1>La recherche "<?php the_search_query(); ?>" n'a retourné aucun résultat</h1>
 
-	<?php endif; ?>
+		<?php endif; ?>
+
+	</div>
 
 <?php get_footer(); ?>
