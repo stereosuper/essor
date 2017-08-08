1e1075c7755a3b21ce08c62a28c4298b49e0a9d2
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-08-08 16:19:04 +0200

    :art: search

--- src/theme/search.php
+++ src/theme/search.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-	<div class='container'>
+	<div class='container-small'>
 
 		<?php if ( have_posts() ) : 
 		global $wp_query;
@@ -8,12 +8,13 @@
 
 			<h1>La recherche "<?php the_search_query(); ?>" a retourné <?php if($results > 1){ echo $results . ' résultats'; }else{ echo '1 résultat'; } ?> </h1>
 
-			<ul>
+			<ul class='search-results'>
 				<?php while ( have_posts() ) : the_post(); ?>
 					<li>
 						<a href='<?php the_permalink(); ?>'>
 							<h2><?php the_title(); ?></h2>
 							<?php the_excerpt(); ?>
+							<span>Consulter<svg class='icon icon-right'><use xlink:href='#icon-right'></use></svg></span>
 						</a>
 					</li>
 				<?php endwhile; ?>
