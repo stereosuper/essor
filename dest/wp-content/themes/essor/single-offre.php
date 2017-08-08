9daa4cb974a70fce634564e724b4ece3e18b87e4
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-08-08 17:51:26 +0200

    :art: content with sidebar

--- dest/wp-content/themes/essor/single-offre.php
+++ dest/wp-content/themes/essor/single-offre.php
@@ -85,73 +85,75 @@ get_header(); ?>
 
 	<?php if ( have_posts() ) : the_post(); ?>
 
-		<div class='container-small'>
-            <aside>
-                <?php wp_nav_menu( array( 'theme_location' => 'jobs', 'container' => false, 'menu_class' => 'menu-aside' ) ); ?>
-            </aside>
+		<div class='container'>
+            <div class='wrapper-content-sidebar'>
+                <aside>
+                    <?php wp_nav_menu( array( 'theme_location' => 'jobs', 'container' => false, 'menu_class' => 'menu-aside' ) ); ?>
+                </aside>
+                <div class='content-sidebar'>
+                    <h1><?php the_title(); ?></h1>
+                    <h2>
+                        <?php echo get_the_terms( $post->ID, 'contrat' )[0]->name; ?>
+                        -
+                        <?php echo get_the_terms( $post->ID, 'lieu' )[0]->name; ?>
+                    </h2>
+                    <?php the_content(); ?>
 
-			<h1><?php the_title(); ?></h1>
-            <h2>
-                <?php echo get_the_terms( $post->ID, 'contrat' )[0]->name; ?>
-                -
-                 <?php echo get_the_terms( $post->ID, 'lieu' )[0]->name; ?>
-            </h2>
-			<?php the_content(); ?>
+                    <h3>Postuler</h3>
 
-            <h3>Postuler</h3>
+                    <div class='form-contact'>
 
-            <div class='form-contact'>
-
-                <?php if( $success ){ ?>
-                    <p class='form-success'>
-                        Merci, votre message a bien été envoyé!
-                        <span>Nous vous répondrons dans les plus bref délais.</span>
-                    </p>
-                <?php }else if( $error ){ ?>
-                    <p class='form-error'>
-                        <?php if( $errorSend ){ ?>
-                            <?php echo $errorSend; ?>
-                        <?php }else{ ?>
-                            <?php if($errorEmpty) echo 'Merci de corriger les erreurs ci-dessous.'; ?>
-                            <span><?php if($errorMailTxt) echo $errorMailTxt; ?></span>
-                            <span><?php if($errorPhoneTxt) echo $errorPhoneTxt; ?></span>
+                        <?php if( $success ){ ?>
+                            <p class='form-success'>
+                                Merci, votre message a bien été envoyé!
+                                <span>Nous vous répondrons dans les plus bref délais.</span>
+                            </p>
+                        <?php }else if( $error ){ ?>
+                            <p class='form-error'>
+                                <?php if( $errorSend ){ ?>
+                                    <?php echo $errorSend; ?>
+                                <?php }else{ ?>
+                                    <?php if($errorEmpty) echo 'Merci de corriger les erreurs ci-dessous.'; ?>
+                                    <span><?php if($errorMailTxt) echo $errorMailTxt; ?></span>
+                                    <span><?php if($errorPhoneTxt) echo $errorPhoneTxt; ?></span>
+                                <?php } ?>
+                            </p>
                         <?php } ?>
-                    </p>
-                <?php } ?>
 
-                <form method='post' action='<?php the_permalink(); ?>#form' class='<?php if( $success ) echo "success"; ?>' id='form-contact'>
-                    <div class='field <?php if($errorName) echo 'error'; ?>'>
-                        <label for='name'>Votre prénom et nom</label>
-                        <input type='text' name='full_name' id='name' value='<?php echo $name; ?>' placeholder='Alain Deloin' required>
+                        <form method='post' action='<?php the_permalink(); ?>#form' class='<?php if( $success ) echo "success"; ?>' id='form-contact'>
+                            <div class='field <?php if($errorName) echo 'error'; ?>'>
+                                <label for='name'>Votre prénom et nom</label>
+                                <input type='text' name='full_name' id='name' value='<?php echo $name; ?>' placeholder='Alain Deloin' required>
+                            </div>
+
+                            <div class='field <?php if($errorMail) echo 'error'; ?>'>
+                                <label for='email'>Votre email</label>
+                                <input type='email' name='email' id='email' value='<?php echo $mail; ?>' placeholder='alain.deloin@laposte.net' required>
+                            </div>
+
+                            <div class='field optional <?php if($errorPhone) echo 'error'; ?>'>
+                                <label for='tel'>Votre numéro de téléphone</label>
+                                <input type='tel' name='tel' id='tel' value='<?php echo $phone; ?>' placeholder='06 00 00 00 00'>
+                                <i>(facultatif)</i>
+                            </div>
+
+                            <div class='field field-top <?php if($errorMsg) echo 'error'; ?>'>
+                                <label for='message'>Votre message</label>
+                                <textarea name='message' id='message' placeholder="J'aime beaucoup ce que vous faites! Laissez moi vous parler de mon incroyabe projet." required><?php echo $msg; ?></textarea>
+                            </div>
+
+                            <div class='hidden'>
+                                <input type='url' name='url' id='url' value='<?php echo $spamUrl; ?>'>
+                                <label for='url'>Merci de laisser ce champ vide.</label>
+                            </div>
+
+                            <button class='btn' type='submit' name='submit' form='form-contact'>
+                                Envoyer
+                                <svg class='icon'><use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#icon-right'></use></svg>
+                            </button>
+                        </form>
                     </div>
-
-                    <div class='field <?php if($errorMail) echo 'error'; ?>'>
-                        <label for='email'>Votre email</label>
-                        <input type='email' name='email' id='email' value='<?php echo $mail; ?>' placeholder='alain.deloin@laposte.net' required>
-                    </div>
-
-                    <div class='field optional <?php if($errorPhone) echo 'error'; ?>'>
-                        <label for='tel'>Votre numéro de téléphone</label>
-                        <input type='tel' name='tel' id='tel' value='<?php echo $phone; ?>' placeholder='06 00 00 00 00'>
-                        <i>(facultatif)</i>
-                    </div>
-
-                    <div class='field field-top <?php if($errorMsg) echo 'error'; ?>'>
-                        <label for='message'>Votre message</label>
-                        <textarea name='message' id='message' placeholder="J'aime beaucoup ce que vous faites! Laissez moi vous parler de mon incroyabe projet." required><?php echo $msg; ?></textarea>
-                    </div>
-
-                    <div class='hidden'>
-                        <input type='url' name='url' id='url' value='<?php echo $spamUrl; ?>'>
-                        <label for='url'>Merci de laisser ce champ vide.</label>
-                    </div>
-
-                    <button class='btn' type='submit' name='submit' form='form-contact'>
-                        Envoyer
-                        <svg class='icon'><use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#icon-right'></use></svg>
-                    </button>
-                </form>
-
+                </div>
             </div>
 		</div>
 	
