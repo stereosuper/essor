7856f2b1b66377e7a7adce96012dd14635cd7b4b
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-08-09 14:12:35 +0200

    :art: dropdown rework

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -2,19 +2,14 @@ var $ = require('jquery');
 
 module.exports = function(dropdowns){
     if( !dropdowns.length ) return;
-    var dropdownButton;
 
-    dropdowns.each(function(index){
-        dropdownButton = $(this).find('.dropdown-title');
-        dropdownButton.on('click', function(e){
-            e.preventDefault();
-            e.stopPropagation();
-            $('.dropdown-title').not(this).parents('.dropdown').removeClass('isOpen');
-            $(this).parents('.dropdown').toggleClass('isOpen');
-        });
-        $('html').click(function() {
-            dropdowns.removeClass('isOpen');
-        });
+    dropdowns.on('click', '.dropdown-title', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).parent().toggleClass('isOpen').siblings().find('.dropdown-title').removeClass('isOpen');
+    });
+    $('html').click(function() {
+        dropdowns.removeClass('isOpen');
     });
     
 }
