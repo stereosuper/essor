b63119cec6d287692e8f63fc44e8540fb515b3bb
ClementLmn <clementlemoine.cl@gmail.com>
2017-08-10 16:32:48 +0200

    :art: new contact page

--- src/theme/contact.php
+++ src/theme/contact.php
@@ -88,63 +88,76 @@ get_header(); ?>
 	<?php if ( have_posts() ) : the_post(); ?>
 
 		<?php the_post_thumbnail('full'); ?>
+		<div class='container'>
+			<div class='wrapper-content-sidebar'>
+				<aside class='wrapper-sticky contact-aside'>
+					<div class='wrapper-aside' id='blockSticky'>
+						<?php the_field('infos'); ?>
+					</div>
+				</aside>
+				<div class='content-sidebar'>
+					<h1><?php the_title(); ?></h1>
+					<div id='form'><?php the_content(); ?></div>
 
-		<div class='container-small'>
-			<h1><?php the_title(); ?></h1>
-			<div id='form'><?php the_content(); ?></div>
+					<div class='form-contact'>
 
-			<div class='form-contact'>
-
-				<?php if( $success ){ ?>
-					<p class='form-success'>
-						Merci, votre message a bien été envoyé!
-						<span>Nous vous répondrons dans les plus bref délais.</span>
-					</p>
-				<?php }else if( $error ){ ?>
-					<p class='form-error'>
-						<?php if( $errorSend ){ ?>
-							<?php echo $errorSend; ?>
-						<?php }else{ ?>
-							<?php if($errorEmpty) echo 'Merci de corriger les erreurs ci-dessous.'; ?>
-							<span><?php if($errorMailTxt) echo $errorMailTxt; ?></span>
-							<span><?php if($errorPhoneTxt) echo $errorPhoneTxt; ?></span>
+						<?php if( $success ){ ?>
+							<p class='form-success'>
+								Merci, votre message a bien été envoyé!
+								<span>Nous vous répondrons dans les plus bref délais.</span>
+							</p>
+						<?php }else if( $error ){ ?>
+							<p class='form-error'>
+								<?php if( $errorSend ){ ?>
+									<?php echo $errorSend; ?>
+								<?php }else{ ?>
+									<?php if($errorEmpty) echo 'Merci de corriger les erreurs ci-dessous.'; ?>
+									<span><?php if($errorMailTxt) echo $errorMailTxt; ?></span>
+									<span><?php if($errorPhoneTxt) echo $errorPhoneTxt; ?></span>
+								<?php } ?>
+							</p>
 						<?php } ?>
-					</p>
-				<?php } ?>
 
-				<form method='post' action='<?php the_permalink(); ?>#form' class='<?php if( $success ) echo "success"; ?>' id='form-contact'>
-					<div class='field <?php if($errorName) echo 'error'; ?>'>
-						<label for='name'>Votre prénom et nom</label>
-						<input type='text' name='full_name' id='name' value='<?php echo esc_attr( $name ); ?>' placeholder='Alain Deloin' required>
+						<form method='post' action='<?php the_permalink(); ?>#form' class='<?php if( $success ) echo "success"; ?>' id='form-contact'>
+							<div class='field <?php if($errorName) echo 'error'; ?>'>
+								<label for='name'>Votre prénom et nom</label>
+								<input type='text' name='full_name' id='name' value='<?php echo esc_attr( $name ); ?>' placeholder='Alain Deloin' required>
+							</div>
+
+							<div class='field <?php if($errorMail) echo 'error'; ?>'>
+								<label for='email'>Votre email</label>
+								<input type='email' name='email' id='email' value='<?php echo esc_attr( $mail ); ?>' placeholder='alain.deloin@laposte.net' required>
+							</div>
+
+							<div class='field optional <?php if($errorPhone) echo 'error'; ?>'>
+								<label for='tel'>Votre numéro de téléphone</label>
+								<input type='tel' name='tel' id='tel' value='<?php echo esc_attr( $phone ); ?>' placeholder='06 00 00 00 00'>
+								<i>(facultatif)</i>
+							</div>
+
+							<div class='field objet <?php if($errorPhone) echo 'error'; ?>'>
+								<label for='objet'>Objet de votre message</label>
+								<input type='text' name='objet' id='objet' value='<?php echo esc_attr( $phone ); ?>' placeholder='Renseignez le sujet de votre correspondance' required>
+							</div>
+
+							<div class='field field-top <?php if($errorMsg) echo 'error'; ?>'>
+								<label for='message'>Votre message</label>
+								<textarea name='message' id='message' placeholder="J'aime beaucoup ce que vous faites! Laissez moi vous parler de mon incroyabe projet." required><?php echo esc_textarea( $msg ); ?></textarea>
+							</div>
+
+							<div class='hidden'>
+								<input type='url' name='url' id='url' value='<?php echo esc_url( $spamUrl ); ?>'>
+								<label for='url'>Merci de laisser ce champ vide.</label>
+							</div>
+
+							<button class='btn' type='submit' name='submit' form='form-contact'>
+								Envoyer
+								<svg class='icon'><use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#icon-right'></use></svg>
+							</button>
+						</form>
+
 					</div>
-
-					<div class='field <?php if($errorMail) echo 'error'; ?>'>
-						<label for='email'>Votre email</label>
-						<input type='email' name='email' id='email' value='<?php echo esc_attr( $mail ); ?>' placeholder='alain.deloin@laposte.net' required>
-					</div>
-
-					<div class='field optional <?php if($errorPhone) echo 'error'; ?>'>
-						<label for='tel'>Votre numéro de téléphone</label>
-						<input type='tel' name='tel' id='tel' value='<?php echo esc_attr( $phone ); ?>' placeholder='06 00 00 00 00'>
-						<i>(facultatif)</i>
-					</div>
-
-					<div class='field field-top <?php if($errorMsg) echo 'error'; ?>'>
-						<label for='message'>Votre message</label>
-						<textarea name='message' id='message' placeholder="J'aime beaucoup ce que vous faites! Laissez moi vous parler de mon incroyabe projet." required><?php echo esc_textarea( $msg ); ?></textarea>
-					</div>
-
-					<div class='hidden'>
-						<input type='url' name='url' id='url' value='<?php echo esc_url( $spamUrl ); ?>'>
-						<label for='url'>Merci de laisser ce champ vide.</label>
-					</div>
-
-					<button class='btn' type='submit' name='submit' form='form-contact'>
-						Envoyer
-						<svg class='icon'><use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#icon-right'></use></svg>
-					</button>
-				</form>
-				
+				</div>
 			</div>
 		</div>
 	
