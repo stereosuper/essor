636b0ec9f7005f22a830c7e7e8d9c684f50b3453
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-08-17 16:40:47 +0200

    :art: about basic styles + list skills

--- dest/wp-content/themes/essor/about.php
+++ dest/wp-content/themes/essor/about.php
@@ -11,14 +11,36 @@ get_header(); ?>
             <div class='wrapper-content-sidebar'>
 
                 <aside class='wrapper-sticky'>
-                    <div class='wrapper-menu-aside' id='blockSticky'>
+                    <div class='wrapper-menu-aside <?php if( url_to_postid( get_field('aboutLink', 'options') ) == $post->ID ){ echo 'menu-no-current'; } ?>' id='blockSticky'>
                         <?php wp_nav_menu( array( 'theme_location' => 'about', 'container' => false, 'menu_class' => 'menu-aside' ) ); ?>
                     </div>
                 </aside>
 
-                <div class='content-sidebar'>
+                <div class='content-sidebar content-about'>
                     <h1><?php the_title(); ?></h1>
                     <?php the_content(); ?>
+
+                    <?php if( have_rows('module') ): ?>
+                        <?php while ( have_rows('module') ) : the_row(); ?>
+
+                            <?php if( get_row_layout() == 'skillsList' ): ?>
+                                <?php if( have_rows('skills') ): ?>
+                                    <ul class='list-skills'>
+                                    <?php while ( have_rows('skills') ) : the_row(); ?>
+                                        
+                                        <li>
+                                            <?php echo wp_get_attachment_image( get_sub_field('logo'), 'full' ); ?>
+                                            <h2><?php the_sub_field('name'); ?></h2>
+                                            <p><?php the_sub_field('text'); ?></p>
+                                        </li>
+
+                                    <?php endwhile; ?>
+                                    </ul>
+                                <?php endif; ?>
+                            <?php endif; ?>
+
+                        <?php endwhile; ?>
+                    <?php endif; ?>
                 </div>
                 
             </div>
