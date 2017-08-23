78df1b60411694d13d510317cf2c4e5d1cc8c9d4
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-08-23 12:43:18 +0200

    :books: update plugin

--- dest/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ dest/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit3ec61e268ab35d8530e8fd9c51c729a4::getLoader();
+return ComposerAutoloaderInit9c8a65cc923b32a9bd35601bc89c1761::getLoader();
