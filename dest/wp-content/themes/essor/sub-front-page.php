05ee8ef1120528fe8e8a0e6bedfd7c38d4ed75d0
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-08-24 14:42:22 +0200

    :art: sub front page

--- dest/wp-content/themes/essor/sub-front-page.php
+++ dest/wp-content/themes/essor/sub-front-page.php
@@ -11,28 +11,33 @@ get_header(); ?>
         $i = 0;
 
         if( $highlightedProjects ): ?>
-            <?php foreach( $highlightedProjects as $post): ?>
-                <?php setup_postdata($post); ?>
-                <div <?php if( $i !== 0 ){ echo 'style="display:none"'; } ?>>
-                    <?php if( function_exists('gacfai_get_field') && gacfai_get_field('img') ){ ?>
-                        <?php echo gacfai_get_field('img', $post->ID, 'full', false, ''); ?>
-                    <?php }else if( has_post_thumbnail() ){ ?>
-                        <?php the_post_thumbnail('full'); ?>
-                    <?php } ?>
-                </div>
-            <?php $i++; endforeach; ?>
-            <?php wp_reset_postdata(); ?>
-        <?php endif; ?>
-        
-        <div class='container'>
-            <?php if( $highlightedProjects ): ?>
+            <div class='slider'>
                 <?php foreach( $highlightedProjects as $post): ?>
                     <?php setup_postdata($post); ?>
-                    <a href='<?php the_permalink(); ?>'><?php the_title(); ?></a>
-                <?php endforeach; ?>
+                    <div <?php if( $i !== 0 ){ echo 'style="display:none"'; } ?>>
+                        <?php if( function_exists('gacfai_get_field') && gacfai_get_field('img') ){ ?>
+                            <?php echo gacfai_get_field('img', $post->ID, 'full', false, ''); ?>
+                        <?php }else if( has_post_thumbnail() ){ ?>
+                            <?php the_post_thumbnail('full'); ?>
+                        <?php } ?>
+                    </div>
+                <?php $i++; endforeach; ?>
                 <?php wp_reset_postdata(); ?>
-            <?php endif; ?>
-        </div>
+
+                <div class='container'>
+                    <ul>
+                        <?php foreach( $highlightedProjects as $post): ?>
+                            <?php setup_postdata($post); ?>
+                            <li><a href='<?php the_permalink(); ?>'>
+                                <?php the_field('place'); ?>
+                                <?php the_title(); ?>
+                            </a></li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <?php wp_reset_postdata(); ?>
+                </div>
+            </div>
+        <?php endif; ?>
 
 		<div class='container-small bloc-title-img' id='blocTitle'>
 			<h1><?php the_title(); ?></h1>
@@ -40,43 +45,47 @@ get_header(); ?>
         </div>
 
         <div class='container'>
-            <?php the_field('text'); ?>
+            <div class='sector-detail'>
+                <div class='sector-text'>
+                    <?php the_field('text'); ?>
+                </div>
 
-            <?php if( have_rows('numbers') ): ?>
-                <ul class='list-numbers'>
-                <?php while( have_rows('numbers') ): the_row(); ?>
+                <?php if( have_rows('numbers') ): ?>
+                    <ul class='list-numbers list-numbers-sector'>
+                    <?php while( have_rows('numbers') ): the_row(); ?>
 
-                    <li class='isAnimated'>
-                        <div>
-                            <?php echo wp_get_attachment_image( get_sub_field('img'), 'full' ); ?>
-                            <b><?php the_sub_field('number'); ?><span><?php the_sub_field('unit'); ?></span></b>
-                        </div>
-                        <?php the_sub_field('text'); ?>
-                        <?php if( get_sub_field('link') && get_sub_field('linkText') ){ ?>
-                            <a href='<?php the_sub_field('link'); ?>' class='link'><?php the_sub_field('linkText'); ?></a>
-                        <?php } ?>
-                    </li>
+                        <li class='isAnimated'>
+                            <div>
+                                <?php echo wp_get_attachment_image( get_sub_field('img'), 'full' ); ?>
+                                <b><?php the_sub_field('number'); ?><span><?php the_sub_field('unit'); ?></span></b>
+                            </div>
+                            <span><?php the_sub_field('text'); ?></span>
+                            <?php if( get_sub_field('link') && get_sub_field('linkText') ){ ?>
+                                <a href='<?php the_sub_field('link'); ?>' class='link'><?php the_sub_field('linkText'); ?></a>
+                            <?php } ?>
+                        </li>
 
-                <?php endwhile; ?>
-                </ul>
-            <?php endif; ?>
+                    <?php endwhile; ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
 
             <?php if( have_rows('people') ): ?>
-                <h2><?php the_field('peopleTitle'); ?></h2>
+                <h2 class='has-margin'><?php the_field('peopleTitle'); ?></h2>
 
-                <ul>
+                <ul class='list-people-sector'>
                 <?php while( have_rows('people') ): the_row(); ?>
 
                     <li class='isAnimated'>
                         <?php the_sub_field('job'); ?>
-                        <?php the_sub_field('name'); ?>
+                        <h3><?php the_sub_field('name'); ?></h3>
                     </li>
 
                 <?php endwhile; ?>
                 </ul>
             <?php endif; ?>
 
-            <h2><?php the_field('projectTitle'); ?></h2>
+            <h2 class='has-margin'><?php the_field('projectTitle'); ?></h2>
             <?php
                 $projectsQuery = new WP_Query(array('post_type' => 'reference', 'posts_per_page' => 4, 'tax_query' => array('taxonomy' => 'batiment', 'field' => 'slug', 'terms' => get_field('sector')), 'post_status' => 'publish'));
 
@@ -90,10 +99,17 @@ get_header(); ?>
                 <?php }
                 wp_reset_query();
             ?>
-            <?php the_field('projectText'); ?>
+        </div>
 
-            <h2><?php the_field('mapTitle'); ?></h2>
-            <?php the_field('mapText'); ?>
+        <div class='container-small'>
+            <?php the_field('projectText'); ?>
+        </div>
+        
+        <div class='container map-sector'>
+            <div class='map-txt-sector'>
+                <h2><?php the_field('mapTitle'); ?></h2>
+                <?php the_field('mapText'); ?>
+            </div>
             <?php echo wp_get_attachment_image( get_field('mapImg'), 'full' ); ?>
         </div>
 	
