8b59b54a70a8ef29835c93354a1a1eafaffdef77
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-08-30 14:49:48 +0200

    :bug: fix #21 js load event

--- src/js/main.js
+++ src/js/main.js
@@ -16,12 +16,12 @@ $(function(){
 
     var animSearchform = require('./animSearchform.js');
     var animResponsiveHeader = require('./animResponsiveHeader.js');
-    var jobsSticky = require('./jobsSticky.js');
     var customDropdown = require('./dropdown.js');
     var loadMorePosts = require('./loadMorePosts.js');
     var initScrollReval = require('./initScrollReveal.js');
     var sticky = require('./sticky.js');
-    
+    var jobsSticky = require('./jobsSticky.js');
+    var setSlider = require('./slider.js');
     $.fn.annotatedImage = require('./annotedImages.js');
 
     var body = $('body');
@@ -39,51 +39,50 @@ $(function(){
         windowHeight = $(window).height();
     }
 
+    function loadHandler(){
+        // Sticky
+        sticky($('#blockSticky'), 130, {minimumWidth: 960});
+        sticky($('#dropdownsSticky'), 0, {minimumWidth: 960});
+
+        // Handle header pushed by filters
+        jobsSticky(body, header, 460, 'page-template-offres', $('#blockStickyJobs'), 960);
+
+        // Slider
+        setSlider( $('#slider') );
+
+        // Annoted images
+        $('.annotated-image').annotatedImage();
+
+        // Load more posts
+        loadMorePosts(wp, $('#ajax-content'));
+    }
+
     // Open and close header searchform
     animSearchform( $('#formSearch') );
 
     // Handle responsive header: burger menus + menus to swipe
     animResponsiveHeader(body, $('#mainNav'), $('#menus'), $('#main'));
-    
-    // Handle header pushed by filters
-    jobsSticky(body, header, 460, 'page-template-offres', $('#blockStickyJobs'), 960);
 
     // Open and close custom dropdowns
     customDropdown(dropdowns);
-
-    // Load more posts
-    loadMorePosts(wp, $('#ajax-content'));
 
     // ScrollReveal
     window.sr = ScrollReveal();
     //window.scrollReveal = new ScrollReveal({ reset: true, scale: 1, distance: '30px', duration: 800, viewFactor: 0.5 });
     initScrollReval('.isAnimated');
 
-    // Sticky
-    sticky($('#blockSticky'), 130, {
-        minimumWidth: 960
-    });
-    sticky($('#dropdownsSticky'), 0, {
-        minimumWidth: 960
-    });
 
-    // Annoted images
-    $('.annotated-image').annotatedImage();
-
+    // Since script is loaded asynchronously, load event isn't always fired !!!
+    if(document.readyState === 'complete'){
+        loadHandler();
+    }else{
+        $(window).on('load', loadHandler);
+    }
 
 
     $(window).on('resize', throttle(function(){
-
         requestAnimFrame(resizeHandler);
-
-    }, 60)).on('load', function(){
-
-        var setSlider = require('./slider.js');
-
-        // Slider
-        setSlider( $('#slider') );
-
-    });
+    }, 60));
 
 
     $(document).on('scroll', throttle(function(){
