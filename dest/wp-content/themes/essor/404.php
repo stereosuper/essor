da3d900d45f1416a3571ebadab9db87519cffb4f
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-10-04 11:15:49 +0200

    :art: 404

--- dest/wp-content/themes/essor/404.php
+++ dest/wp-content/themes/essor/404.php
@@ -3,7 +3,7 @@
 	<div class='container-tiny'>
 
 		<h1>404</h1>
-		<p>Il semble que cette page n'existe pas ou plus.</p>
+		<p>Oups ! Cette page n'existe pas encore... ou plus du tout. <br>Et si vous retentiez votre chance avec une autre ?</p>
 		<p>
 			<a href='./' class='link'>Retour à l'accueil</a><br>
 			<a href='<?php the_field('refsLink', 'options'); ?>' class='link'>Voir nos références</a>
